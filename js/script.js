fd9eb2abf008ba51e6a7de3442796b8b46caf0b1
Yoni Pinto <yoni.b.pinto@gmail.com>
2015-11-23 21:27:09 -0600

    Responsive CSS, titles, some images
    - Added responsive css for different screen widths
    - Added a few missing headshots/changed ones
    - Added image hover actions (showing funny imgs)
    - Changed section titles to images rather than installing font
    - Added clear at the end of about div as well

--- js/script.js
+++ js/script.js
@@ -12,60 +12,72 @@ $(document).ready(function() {
 	    }, 600, 'swing');
 	});
 
-	// Biopics arrays
-	var team = [];
-
-	var exec = [["Radhika Kalra", "img/headshots/radhika"],
+	// Biopics array
+	var team = [["Radhika Kalra", "img/headshots/radhika"],
 				["Adyut Khazanchi", "img/headshots/adyut"],
 				["Levin Zhu", "img/headshots/levin"],
 				["Sofia Rada", "img/headshots/sofiarada"],
 				["Aashrey Tiku", "img/headshots/aashrey"],
-				["Edwin Argueta", "img/headshots/placeholder"],
+				["Edwin Argueta", "img/headshots/edwin"],
 				["Milaap Mehta", "img/headshots/milaap"],
 				["Mihir Swaroop", "img/headshots/mihir"],
 				["Armaan Shah", "img/headshots/armaan"],
 				["Ashna Mehta", "img/headshots/ashna"],				
 				["Aruj Chaudhry", "img/headshots/aruj"],
-				["Abhi Shekhawat", "img/headshots/placeholder"],
-				["Chris Ebhogiaye", "img/headshots/placeholder"],
-				["Akhil Shanishetti", "img/headshots/placeholder"],
-				["Ashwin Kulkarni", "img/headshots/placeholder"],
-				["Emma Lane", "img/headshots/placeholder"],
-				["Shikhar Soni", "img/headshots/placeholder"],
-				["Aman Parikh", "img/headshots/placeholder"],
-				["Iris Pinto", "img/headshots/placeholder"],
-				["Chaitra Subramaniam", "img/headshots/placeholder"],
-				["Peter Beer", "img/headshots/placeholder"],
-				["Samantha Cohen", "img/headshots/placeholder"],
-				["Zain Syedain", "img/headshots/placeholder"],
-				["Anna Kubacsek", "img/headshots/placeholder"],
-				["Matthew Celentano", "img/headshots/placeholder"],
-				["Kristian Farinha", "img/headshots/placeholder"],
-				["David Lee", "img/headshots/placeholder"],
-				["Catherine Kang", "img/headshots/placeholder"],
-				["Alisha Gandhi", "img/headshots/placeholder"],
-				["Justine Kim", "img/headshots/placeholder"],
-				["Astrid Goh", "img/headshots/placeholder"],
-				["Cameron Braverman", "img/headshots/placeholder"],
-				["Ellis Yun", "img/headshots/placeholder"],
-				["Ankita Chowdhry", "img/headshots/placeholder"],
-				["Mustafa Alimumal", "img/headshots/placeholder"],
-				["Bee Nirundonpruk", "img/headshots/placeholder"],				
-				["Sofia Lopez Franco", "img/headshots/placeholder"],
-				["Gabriel Rojas-Westall", "img/headshots/placeholder"],
-				["Rafi Wasserman", "img/headshots/placeholder"],
-				["Esteban Mercado", "img/headshots/placeholder"],
-				["Jaiveer Kothari", "img/headshots/placeholder"],
-				["Yoni Pinto", "img/headshots/placeholder"]];
+				["Abhi Shekhawat", "img/headshots/abhi"],
+				["Chris Ebhogiaye", "img/headshots/chris"],
+				["Akhil Shanishetti", "img/headshots/akhil"],
+				["Ashwin Kulkarni", "img/headshots/ashwin"],
+				["Emma Lane", "img/headshots/emma"],
+				["Shikhar Soni", "img/headshots/shikhar"],
+				["Aman Parikh", "img/headshots/aman"],
+				["Iris Pinto", "img/headshots/iris"],
+				["Chaitra Subramaniam", "img/headshots/chaitra"],
+				["Peter Beer", "img/headshots/peter"],
+				["Samantha Cohen", "img/headshots/sam"],
+				["Zain Syedain", "img/headshots/zain"],
+				["Anna Kubacsek", "img/headshots/anna"],
+				["Matthew Celentano", "img/headshots/matt"],
+				["Kristian Farinha", "img/headshots/kristian"],
+				["David Lee", "img/headshots/david"],
+				["Catherine Kang", "img/headshots/catherine"],
+				["Alisha Gandhi", "img/headshots/alisha"],
+				["Justine Kim", "img/headshots/justine"],
+				["Astrid Goh", "img/headshots/astrid"],
+				["Cameron Braverman", "img/headshots/cameron"],
+				["Ellis Yun", "img/headshots/ellis"],
+				["Ankita Chowdhry", "img/headshots/ankita"],
+				["Mustafa Alimumal", "img/headshots/mustafa"],
+				["Bee Nirundonpruk", "img/headshots/bee"],				
+				["Sofia Lopez Franco", "img/headshots/slf"],
+				["Gabriel Rojas-Westall", "img/headshots/gabe"],
+				["Rafi Wasserman", "img/headshots/rafi"],
+				["Esteban Mercado", "img/headshots/esteban"],
+				["Jaiveer Kothari", "img/headshots/jaiveer"],
+				["Yoni Pinto", "img/headshots/yoni"]];
 
 	// Biopics addition code
-	for (var i = 0; i < exec.length; i++) {
-		$("#exec").append(
-			"<div class=\"exec-pic-div\"> <img class=\"exec-pic-img\" src=\"" + exec[i][1] + ".jpg\"><p class=\"pic-name team-content-color\">" + exec[i][0] + "</p></div>");
-	}
 
 	for (var i = 0; i < team.length; i++) {
-		$("#team-members").append("<div class=\"ipa-pic-div\"><img class=\"ipa-pic-img\" src=\"" + team[i][1] + "\"><p class=\"pic-name team-content-color\">" + ipas[i][0] + "</p></div>");
+		$("#team-members").append(
+			"<div class=\"team-pic-div\"><img class=\"team-pic-img\" src=\"" + team[i][1] + ".jpg\"><p class=\"pic-name team-content-color\">" + team[i][0] + "</p></div>");
 	}
+
+	$(".team-pic-img").hover(
+		function() {
+			var imgSrc = $(this).attr("src");
+			var newSrc = imgSrc.substring(0, imgSrc.indexOf(".")) + "2.jpg";
+
+			$(this).attr("src", newSrc);
+		}, 
+		function() {
+			var imgSrc = $(this).attr("src");
+			var newSrc = imgSrc.substring(0, imgSrc.search("2")) + ".jpg";
+
+			$(this).attr("src", newSrc);
+		}
+	);
+
+	
 
 });
